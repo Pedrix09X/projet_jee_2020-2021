26d48429af2a0700d97b44d1fefac12d885dce6a
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-12-23 18:40:05 +0100

    Petit testsupplémentaire dans Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.sql.SQLException;
+import java.util.List;
+
+import entities.Entity;
+import tables.UserTable;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Object str = "";
-		System.out.println(str.getClass().getTypeName());
+		UserTable userTable = new UserTable();
+		try {
+			List<Entity> users = userTable.getAll();
+			for (Entity user : users) {
+				System.out.println(user.toString());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
