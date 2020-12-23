7c43b037b433ed893ac35d693f273a8a6ea938f1
Aude Marêché <aude.mareche@free.fr>
2020-12-23 17:01:15 +0100

    Normalisation BDD

--- tables.sql
+++ tables.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE antiCovid;
+use antiCovid;
+
 CREATE TABLE User (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     login TEXT(20) NOT NULL UNIQUE,
@@ -19,30 +22,26 @@ CREATE TABLE Location (
 
 CREATE TABLE Activity (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user INT NOT NULL,
     title TEXT(100) NOT NULL,
     startDate DATE NOT NULL DEFAULT NOW(),
     endDate DATE NOT NULL DEFAULT NOW(),
     location INT NOT NULL,
-    FOREIGN KEY (location) REFERENCES Location(id)
-);
-
-CREATE TABLE User_Activity (
-    user INT NOT NULL,
-    activity INT NOT NULL,
     FOREIGN KEY (user) REFERENCES User(id),
-    FOREIGN KEY (activity) REFERENCES Activity(id)
+    FOREIGN KEY (location) REFERENCES Location(id)
 );
 
 CREATE TABLE Friends (
     user INT NOT NULL,
     friend INT NOT NULL CHECK ( friend <> user ),
+    PRIMARY KEY (user, friend),
     FOREIGN KEY (user) REFERENCES User(id),
     FOREIGN KEY (friend) REFERENCES User(id)
 );
 
 CREATE TABLE Notification (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    text TEXT(255) NOT NULL DEFAULT 'Pas de text',
+    text TEXT(255) NOT NULL DEFAULT 'Pas de texte',
     receivedDate DATE NOT NULL DEFAULT NOW(),
     user INT NOT NULL,
     FOREIGN KEY (user) REFERENCES User(id)
