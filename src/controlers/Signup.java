45f0139e74876af7c61ab596e0764164ebce4d15
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-12-28 21:33:50 +0100

    Création de la page d'inscription (servlet+jsp+web.xml)

---
+++ src/controlers/Signup.java
@@ -0,0 +1,38 @@
+package controlers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Signup")
+public class Signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Signup() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("title", "antiCovid - Inscription");
+		request.setAttribute("page", "signup.jsp");
+		request.getRequestDispatcher("jsp/default.jsp").forward(request, response);
+		request.getRequestDispatcher("jsp/signup.jsp").include(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
